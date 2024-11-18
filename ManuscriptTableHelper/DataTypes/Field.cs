10bb84fb68fe5394fd737781c2defeccbc755d92
Dakota M <PapaBeans@users.noreply.github.com>
2024-11-18 12:12:28 -0500

    gitignore

--- ManuscriptTableHelper/DataTypes/Field.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ManuscriptTableHelper.DataTypes
-{
-    public class Field
-    {
-        public string Type { get; set; }
-        public string Name { get; set; }
-    }
-}
--- ManuscriptTableHelper/DataTypes/Key.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ManuscriptTableHelper.DataTypes
-{
-    public class Key
-    {
-        public string Value { get; set; }
-        public bool IsDefault { get; set; }
-        public string Caption { get; set; }
-    }
-}
